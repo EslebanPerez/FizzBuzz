43694288b3da3fc8ac93ba3a9cac4b8a22097a79
EslebanPerez <eslebanperez@gmail.com>
2022-05-02 00:39:33 -0500

    Agregando prueba para valor divicible entre 3

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -6,5 +6,12 @@ describe('Test para FizzbuzzService', () =>{
         FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
         expect(explorer1.trick).toBe(1)
     })
+    test('Prueba 2: Asigna FIZZ si el score es divisible entre 3', () =>{
+        const explorer3 = {name: "Explorer3", score: 3}
+        FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+        expect(explorer3.trick).toBe("FIZZ")
+    })
+ 
+    
 
 })
